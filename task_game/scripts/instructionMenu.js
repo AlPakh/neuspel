e427ec63174461cf30037fc5161920108847057e
AlPakh <alvp2003@yandex.ru>
2024-03-11 02:21:14 +0300

    Changed instruction menu

--- task_game/scripts/instructionMenu.js
+++ task_game/scripts/instructionMenu.js
@@ -1,5 +1,13 @@
 let curInstPage = 1;
-const totInstPages = 3;
+var instructionNotes = [ "",
+    "Здравствуйте! \nПеред вами «Буревестник» — браузерная игра, созданная, чтобы проверить Ваши навыки ориентирования во времени.",
+    "Уровни \nВ этой игре Вам будет представлены две мини-игры. У каждой из них есть три уровня сложности. Когда вы пройдёте обе миниигры на каждом уровне сложности. \nВы сможете выбирать уровень с которого хотите продолжить начиная со второго запуска",
+    "Никнейм \n«Буревестник» — название не просто игры, а Вашего космического корабля. Однако, вы можете менять его, если вы уже посетили уровни. Для выбора нового имени, кликните на название игры в главном меню. Не забудьте сохранить введённое имя!",
+    "Слушайте\n Звуками в мини-играх обозначаются ваши успехи, промахи, а также появление объектов на экране. Если на слух у вас реакция быстрее, не забудьте включить звук!",
+    "Первая мини-игра — «Индикаторы». \nНа экране синим цветом загорается один из трёх индикаторов; в это время на нём появляется число. Вам необходимо нажать на индикатор через столько секунд, сколько изображено на индикаторе. Если вы успели кликнуть, индикатор загорится зелёным. если нет — красным",
+    "Вторая мини-игра — «QTE». \nНа экране случайно появляются зелёные панельки с цифрами. Вам необходимо нажать на соответствующую клавишу на клавиатуре настолько быстро, насколько возможно. Не перепутайте нужную панельку с обманкой!"
+];
+const totInstPages = instructionNotes.length - 1;
 
 function updatePageIndicator(currentPage, totalPages) {
     let indicator = '';
@@ -10,8 +18,8 @@ function updatePageIndicator(currentPage, totalPages) {
 }
 
 function updateInstructionScreen() {
-    document.getElementById("instruction-text").innerText = `Инструкция: Страница ${curInstPage}`;
-    document.getElementById("instruction-image").src = `image${curInstPage}.jpg`; // Предполагается, что есть image1.jpg, image2.jpg, image3.jpg
+    document.getElementById("instruction-text").innerText = instructionNotes[curInstPage];
+    document.getElementById("instruction-image").src = `img/image${curInstPage}.png`; // Предполагается, что есть image1.jpg, image2.jpg, image3.jpg
     document.getElementById("instruction-page-indicator").innerText = updatePageIndicator(curInstPage, totInstPages);
     if (curInstPage > 1 && curInstPage < totInstPages) {
         document.getElementById("prev-instruction").style.opacity = 1;
